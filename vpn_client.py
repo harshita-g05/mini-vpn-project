d575f1157dd2c8c89a3407b132f77fe4b7fb8671
Harshita Gupta <harshitagupta135@gmail.com>
2025-05-23 21:27:38 -0500

    Finished CLI version of VPN client and server

---
+++ vpn_client.py
@@ -0,0 +1,36 @@
+
+
+import socket #load networking module (creates tcp/udp connections)
+from cryptography.fernet import Fernet
+
+# Load thr same shared key as server
+with open("key.key", "rb") as f:
+    key = f.read()
+fernet = Fernet(key)
+
+# Connect to the server
+host = "127.0.0.1"  #set server ip address to same computer 
+port = 8080 
+
+
+#creating a tcp socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+try:
+    print("Attempting to connect to server...")
+    client.connect((host, port))
+    print("[+] Connected to VPN server")
+except Exception as e:
+    print("Failed to connect:", e)
+
+
+
+# Send/receive loop
+while True:
+    msg = input("Send to server: ") #get user input 
+    encrypted_msg = fernet.encrypt(msg.encode())
+    client.send(encrypted_msg) #encrypt and send msg
+
+    encrypted_reply = client.recv(4096) #wait for server response
+    print(f"[SERVER]: {fernet.decrypt(encrypted_reply).decode()}") #decrypt server reply
+
+client.close()
---
+++ vpn_server.py
@@ -0,0 +1,40 @@
+
+
+# our server needs to do 2 things 
+# (1) handle networking
+# (2) handle encryption 
+
+import socket
+from cryptography.fernet import Fernet
+
+# Load the same shared key as client
+with open("key.key", "rb") as f:
+    key = f.read()
+fernet = Fernet(key)
+
+
+#set up the server to listen
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+server.bind(("0.0.0.0", 8080)) #think of this like "hey computer i wanna listen on this port"
+server.listen(1)
+print("[+] Server is listening, waiting for a client to connect...")
+
+
+#accept an incoming connection from a client.
+conn, addr = server.accept()
+print(f"[DEBUG] Accepted connection from {addr}")
+print(f"[+] Connection established with {addr}")
+
+
+while True:
+    #Receive & Decrypt Message
+    encrypted_msg = conn.recv(4096)
+    decrypted_msg = fernet.decrypt(encrypted_msg)
+    print(f"[CLIENT]: {decrypted_msg.decode()}")
+
+    #Reply Back to the Client
+    msg = input("Send to client: ") 
+    encryptmsg = fernet.encrypt(msg.encode())
+    conn.send(encryptmsg)
+
+
